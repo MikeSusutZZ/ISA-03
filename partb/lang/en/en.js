f5d7b1fb8800a313ef0737c1e1945d6170f87f68
MikeSusutZZ <121924073+MikeSusutZZ@users.noreply.github.com>
2024-01-31 12:03:55 -0800

    Delete partb directory

--- partb/lang/en/en.js
+++
@@ -1,4 +0,0 @@
-// lang/en/en.js
-module.exports = {
-    greeting: "Hello %1, What a beautiful day. Server current date and time is"
-};
--- partb/modules/utils.js
+++
@@ -1,9 +0,0 @@
-// ./modules/utils.js
-const en = require('../lang/en/en');
-
-function getDate(name) {
-    const date = new Date();
-    return en.greeting.replace('%1', name) + ' ' + date.toString();
-}
-
-module.exports = { getDate };
