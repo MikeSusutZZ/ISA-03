05e961c465898ffe6276ff1ef6b750c980ba8344
MikeSusutZZ <121924073+MikeSusutZZ@users.noreply.github.com>
2024-01-31 11:55:22 -0800

    Add files via upload

--- partb/server.js
+++ partb/server.js
@@ -1,11 +1,13 @@
-// server.js
 const http = require('http');
 const url = require('url');
 const utils = require('./modules/utils');
-const port = 3000; // You can choose your port
+
+// Set the port to the environment variable PORT, or 3000 if it's not set
+const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
     const queryObject = url.parse(req.url, true).query;
+    
     if (queryObject.name) {
         const greeting = utils.getDate(queryObject.name);
         res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -14,6 +16,6 @@ http.createServer((req, res) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello, please provide a name.');
     }
-}).listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+}).listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
 });
